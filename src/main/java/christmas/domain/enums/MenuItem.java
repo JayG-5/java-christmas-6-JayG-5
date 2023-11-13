7e10adfad139ae4c6652a46758c0126e55eb1a0f
JayG-5 <jg55552758@gmail.com>
2023-11-13 23:40:18 +0900

    feat: MenuItem Enum 추가

---
+++ src/main/java/christmas/domain/enums/MenuItem.java
@@ -0,0 +1,33 @@
+package christmas.domain.enums;
+
+public enum MenuItem {
+    // 애피타이저
+    YANGSONGHI_SOUP("양송이수프", 6000, Category.APPETIZER.getType()),
+    TAPAS("타파스", 5500, Category.APPETIZER.getType()),
+    CAESAR_SALAD("시저샐러드", 8000, Category.APPETIZER.getType()),
+
+    // 메인
+    T_BONE_STEAK("티본스테이크", 55000, Category.MAIN.getType()),
+    BBQ_RIBS("바비큐립", 54000, Category.MAIN.getType()),
+    SEAFOOD_PASTA("해산물파스타", 35000, Category.MAIN.getType()),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, Category.MAIN.getType()),
+
+    // 디저트
+    CHOCOLATE_CAKE("초코케이크", 15000, Category.DESSERT.getType()),
+    ICE_CREAM("아이스크림", 5000, Category.DESSERT.getType()),
+
+    // 음료
+    ZERO_COLA("제로콜라", 3000, Category.BEVERAGE.getType()),
+    RED_WINE("레드와인", 60000, Category.BEVERAGE.getType()),
+    CHAMPAGNE("샴페인", 25000, Category.BEVERAGE.getType());
+
+    private final String name;
+    private final int price;
+    private final int category;
+
+    MenuItem(String name, int price, int category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}

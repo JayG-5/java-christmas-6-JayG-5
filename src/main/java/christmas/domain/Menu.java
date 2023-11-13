9b5f7241ed799cf4f1f9efea648d871c414db381
JayG-5 <jg55552758@gmail.com>
2023-11-13 23:07:47 +0900

    feat: Menu Class 추가

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,15 @@
+package christmas.domain;
+
+public class Menu {
+    private final String name;
+    private final int price;
+    private final int unit;
+    private final int category;
+
+    public Menu(String name, int unit) {
+        this.name = name;
+        this.unit = unit;
+    }
+
+
+}

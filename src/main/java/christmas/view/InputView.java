bd8c088d5d37bd1752c93bb942bd928a8a3172c7
JayG-5 <jg55552758@gmail.com>
2023-11-15 01:58:40 +0900

    fix: 메서드 자료형 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,16 +3,18 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 import christmas.utils.Parser;
 
+import java.util.List;
+
 public class InputView {
-    public int readDate() {
+    public static int readDate() {
         OutputView.print(Message.REQUEST_DATE.getMessage());
         String input = Console.readLine();
         return Parser.stringToInt(input);
     }
 
-    public  String readMenu(){
+    public static List<String> readMenu(){
         OutputView.print(Message.REQUEST_MENU.getMessage());
         String input = Console.readLine();
-        return input;
+        return Parser.splitToList(input,",");
     }
 }

2f21778388d741e9611d55e792b86e7d3d591ca8
JayG-5 <jg55552758@gmail.com>
2023-11-15 01:15:04 +0900

    feat: Benefit enum 필드 추가

--- src/main/java/christmas/domain/enums/Benefit.java
+++ src/main/java/christmas/domain/enums/Benefit.java
@@ -1,5 +1,22 @@
 package christmas.domain.enums;
 
+import christmas.domain.benefit.*;
+
+
 public enum Benefit {
-    CRISTMASBENEFIT
+    CHRISTMAS(ChristmasBenefit.getInstance(),100),
+    WEEKDAY(WeekdayBenefit.getInstance(),2023),
+    WEEKEND(WeekendBenefit.getInstance(),2023),
+    SPACIAL(SpacialBenefit.getInstance(),1000);
+
+    private final BenefitType benefit;
+    private final int value;
+    Benefit(BenefitType benefit,int value) {
+        this.benefit = benefit;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }

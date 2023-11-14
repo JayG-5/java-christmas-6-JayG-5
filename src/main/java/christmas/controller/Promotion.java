d6d59cb1abbe7a63b50df9a8d64fb3fb649ef911
JayG-5 <jg55552758@gmail.com>
2023-11-15 02:11:34 +0900

    feat: createMenu 메서드 추가

--- src/main/java/christmas/controller/Promotion.java
+++ src/main/java/christmas/controller/Promotion.java
@@ -1,6 +1,9 @@
 package christmas.controller;
 
+import christmas.domain.Menu;
 import christmas.domain.benefit.BenefitType;
+import christmas.utils.Parser;
+import christmas.view.InputView;
 
 import java.util.List;
 import java.util.Map;
@@ -9,12 +12,24 @@ import java.util.Objects;
 public class Promotion {
 
     private final List<Integer> stars;
-//    private int date;
+    //    private int date;
 //    private List<Menu> menus;
 //    private List<BenefitType> benefits;
     private Map<String, Objects> result;
 
     public Promotion(List<Integer> stars) {
         this.stars = stars;
+    }
+
+    private Menu createMenu(int date, String orderText) {
+        final List<String> order = Parser.splitToList(orderText, "-");
+        return new Menu(date, order.get(0), Parser.stringToInt(order.get(1)));
+    }
+
+    public void order() {
+        final int date = InputView.readDate();
+        final List<Menu> menus = InputView.readMenu().stream().map(menuText -> createMenu(date, menuText)).toList();
+
+
     }
 }

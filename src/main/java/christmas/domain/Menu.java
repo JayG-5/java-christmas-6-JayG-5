beede30ce768e48e5364be03dc214fe2482f0a1a
JayG-5 <jg55552758@gmail.com>
2023-11-15 01:24:20 +0900

    fix: 혜택 리스트 수정

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,6 +1,7 @@
 package christmas.domain;
 
 import christmas.domain.benefit.BenefitType;
+import christmas.domain.enums.Benefit;
 import christmas.domain.enums.MenuItem;
 
 import java.util.List;
@@ -11,8 +12,7 @@ public class Menu {
     private final int price;
     private final int unit;
     private final int category;
-
-    private List<BenefitType> benefits;
+    private final List<BenefitType> benefits;
 
     public Menu(int date, String name, int unit) {
         this.name = name;
@@ -20,10 +20,6 @@ public class Menu {
 
         this.price = MenuItem.getPriceByName(name);
         this.category = MenuItem.getCategoryByName(name);
-
-    }
-
-    private void setBenefit(int date){
-
+        this.benefits = Benefit.getMenuBenefit(date,category,unit);
     }
 }

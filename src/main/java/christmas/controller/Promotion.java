1aaf8d6ec380a9b60e861464d1a5bdab9a35a4df
JayG-5 <jg55552758@gmail.com>
2023-11-15 14:54:59 +0900

    feat: 프로모션 진행 컨트롤러 추가 / preprocessBenefitInfo 메서드 추가

--- src/main/java/christmas/controller/Promotion.java
+++ src/main/java/christmas/controller/Promotion.java
@@ -2,17 +2,16 @@ package christmas.controller;
 
 import christmas.domain.Menu;
 import christmas.domain.benefit.BenefitType;
+import christmas.domain.enums.Badge;
+import christmas.domain.enums.Benefit;
 import christmas.utils.Parser;
 import christmas.view.InputView;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class Promotion {
 
     private final List<Integer> stars;
-//    private List<BenefitType> benefits;
     private Map<String, Objects> result;
 
     public Promotion(List<Integer> stars) {
@@ -24,11 +23,29 @@ public class Promotion {
         return new Menu(date, order.get(0), Parser.stringToInt(order.get(1)));
     }
 
+    private Map<String, Object> preprocessBenefitInfo(
+            Map<Class<? extends BenefitType>, Map<String, Object>> benefitInfo) {
+        Map<String, Object> benefitInfoMap = new HashMap<>();
+        Map<String, Object> totalBenefit = new HashMap<>();
+        benefitInfo.forEach((k, benefit) -> {
+            totalBenefit.put((String) benefit.get("name"), (int) benefit.get("value") * (int) benefit.get("count"));
+            if (benefit.containsKey("gift")) benefitInfoMap.put("gift", benefit.get("gift"));
+        });
+        final int totalBenefitValue =  totalBenefit.values().stream().mapToInt(value -> (int) value).sum();
+        benefitInfoMap.put("total_benefit_value",totalBenefitValue);
+        benefitInfoMap.put("total_benefit", totalBenefit);
+        benefitInfoMap.put("badge", Badge.fromPrice(totalBenefitValue));
+        return benefitInfoMap;
+    }
+
     public void order() {
         final int date = InputView.readDate();
         final List<Menu> menus = InputView.readMenu().stream().map(menuText -> createMenu(date, menuText)).toList();
-        menus.stream().mapToInt(Menu::getPrice).sum();
-
-
+        final int totalPrice = menus.stream().mapToInt(Menu::getPrice).sum();
+        List<BenefitType> benefits = Benefit.getPromotionBenefit(stars, date, totalPrice);
+        menus.forEach(menu -> benefits.addAll(menu.getBenefits()));
+        Map<String, Object> benefitInfo = preprocessBenefitInfo(Benefit.getBenefitInfo(benefits));
+        benefitInfo.put("menus",menus);
+        benefitInfo.put("total_price",totalPrice);
     }
 }

5b873cbddf0770bbc227ae4f43578943008fba4f
JayG-5 <jg55552758@gmail.com>
2023-11-15 17:11:35 +0900

    feat: 최소주문금액 체커 추가

--- src/main/java/christmas/utils/Checker.java
+++ src/main/java/christmas/utils/Checker.java
@@ -1,12 +1,20 @@
 package christmas.utils;
 
+import christmas.domain.Menu;
 import christmas.exception.Message;
 import christmas.exception.PromotionException;
+
+import java.util.List;
 
 public class Checker {
     public static void isBetween1And31(int number) {
         if (number < 1 || number > 31) {
             throw PromotionException.of(Message.NUMBER);
+        }
+    }
+    public static void isOverMinimumPrice(List<Menu> menus) {
+        if (menus.stream().mapToInt(Menu::getPrice).sum() < 10000) {
+            throw PromotionException.of(Message.ORDER);
         }
     }
 }

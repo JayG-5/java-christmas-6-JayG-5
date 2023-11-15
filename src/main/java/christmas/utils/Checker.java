7154ba1b4846648df2f889c8c204bc50d2a9f26c
JayG-5 <jg55552758@gmail.com>
2023-11-15 17:16:18 +0900

    feat: 음료 단일주문 체커 추가

--- src/main/java/christmas/utils/Checker.java
+++ src/main/java/christmas/utils/Checker.java
@@ -1,10 +1,13 @@
 package christmas.utils;
 
 import christmas.domain.Menu;
+import christmas.domain.enums.Category;
 import christmas.exception.Message;
 import christmas.exception.PromotionException;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Checker {
     public static void isBetween1And31(int number) {
@@ -14,6 +17,15 @@ public class Checker {
     }
     public static void isOverMinimumPrice(List<Menu> menus) {
         if (menus.stream().mapToInt(Menu::getPrice).sum() < 10000) {
+            throw PromotionException.of(Message.ORDER);
+        }
+    }
+    public static void isOnlyCategory(List<Menu> menus,int category) {
+        Set<Integer> uniqueCategories = menus.stream()
+                .map(Menu::getCategory)
+                .collect(Collectors.toSet());
+
+        if (uniqueCategories.size() == 1 && uniqueCategories.contains(category)) {
             throw PromotionException.of(Message.ORDER);
         }
     }

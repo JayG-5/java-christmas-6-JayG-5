05d05ae6732dc6420c37680a4578a78f8e3530af
JayG-5 <jg55552758@gmail.com>
2023-11-15 15:23:39 +0900

    feat: 프로그램 실행 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.controller.Promotion;
+
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final Promotion promotion = new Promotion(Arrays.asList(3,10,17,24,25,31));
+        promotion.order();
     }
 }
